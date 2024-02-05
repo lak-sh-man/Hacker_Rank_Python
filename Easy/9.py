137d968e42677bdf8e5d0d2b260ae49a552761e2
lak-sh-man <lakshman1122000@gmail.com>
2024-02-05 19:09:36 +0530

    first commit

---
+++ Easy/9.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    x = max(arr)
+    c = arr.count(x)
+
+    for i in range(0, c):
+        arr.remove(x)
+
+    print(max(arr))
